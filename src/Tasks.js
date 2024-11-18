abe2b79e20f9a27f32eea8d5d3bc52a058bb3fe9
admin <admin@home.com>
2024-11-18 21:49:55 +0700

    remove redundant stuffs

--- src/Tasks.js
+++ src/Tasks.js
@@ -9,12 +9,11 @@ const Tasks = () => {
                 <h1>My Embedded Image</h1>
                 <p>This is an example of HTML content with embedded image data.</p>
 
-                <div className="image-container">
                     <img 
                         src="https://firebasestorage.googleapis.com/v0/b/text2image-118de.appspot.com/o/Test%2FFSL.png?alt=media&token=1c0da5c9-e748-4916-96b5-d28ff99e7a6a"
                         alt="Sample Image" 
+                        style={{minWidth: '337px', width: '100%'}}
                     />
-                </div>
 
                 <p>More text content goes here.</p>
                 </div>
