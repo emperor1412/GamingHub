21475e3e691e2b2de4f7e256f2750e528451dee5
admin <admin@home.com>
2024-11-19 22:52:42 +0700

    code polish

--- src/Profile.js
+++ src/Profile.js
@@ -33,51 +33,10 @@ const Profile = ({ onClose,  user, loginData }) => {
 
   return (
     <div className="app-container">
-      <div className="profile-container">
-        <div className="profile-header">
-          <button className="profile-back" onClick={onClose}>
-            <img src={backIcon} alt="Back" />
-          </button>
-          <div className="profile-user">
-            <img 
-              src={avatars[0]?.src} 
-              alt="Avatar" 
-              className="profile-avatar"
-            />
-            <div>
-              <div className="profile-username">RUNNER</div>
-              <div className="profile-id">4CH4H9W2...AaCFChF</div>
-            </div>
-            <button className="profile-user-arrow" onClick={() => setShowAvatarSelector(true)}>
-              <img src={arrowIcon} alt="Open Avatar Selector" />
-            </button>
-          </div>
-        </div>
+      
 
-        <div className="profile-content-wrapper">
-          <div className="profile-content">
-            {profileItems.map((item, index) => (
-              <div key={index} className="profile-item">
-                <div className="profile-item-left">
-                  <img src={item.icon} alt="" className="profile-item-icon" />
-                  <span className="profile-item-text">{item.text}</span>
-                </div>
-                <div className="profile-item-right">
-                  {item.showClaim && (
-                    <button className="profile-claim">Claim</button>
-                  )}
-                  <span className="profile-item-value">{item.value}</span>
-                </div>
-                {item.showArrow && (
-                  <img src={arrowIcon} alt="" className="profile-item-arrow" />
-                )}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-
-      {showAvatarSelector && (
+      {
+      showAvatarSelector ? (
         <ProfileAvatarSelector 
           onClose={() => setShowAvatarSelector(false)}
           onSelect={(avatar) => {
@@ -85,7 +44,51 @@ const Profile = ({ onClose,  user, loginData }) => {
           }}
           user={user}
         />
-      )}
+      )
+     : <div className="profile-container">
+     <div className="profile-header">
+       <button className="profile-back" onClick={onClose}>
+         <img src={backIcon} alt="Back" />
+       </button>
+       <div className="profile-user">
+         <img 
+           src={avatars[0]?.src} 
+           alt="Avatar" 
+           className="profile-avatar"
+         />
+         <div>
+           <div className="profile-username">RUNNER</div>
+           <div className="profile-id">4CH4H9W2...AaCFChF</div>
+         </div>
+         <button className="profile-user-arrow" onClick={() => setShowAvatarSelector(true)}>
+           <img src={arrowIcon} alt="Open Avatar Selector" />
+         </button>
+       </div>
+     </div>
+
+     <div className="profile-content-wrapper">
+       <div className="profile-content">
+         {profileItems.map((item, index) => (
+           <div key={index} className="profile-item">
+             <div className="profile-item-left">
+               <img src={item.icon} alt="" className="profile-item-icon" />
+               <span className="profile-item-text">{item.text}</span>
+             </div>
+             <div className="profile-item-right">
+               {item.showClaim && (
+                 <button className="profile-claim">Claim</button>
+               )}
+               <span className="profile-item-value">{item.value}</span>
+             </div>
+             {item.showArrow && (
+               <img src={arrowIcon} alt="" className="profile-item-arrow" />
+             )}
+           </div>
+         ))}
+       </div>
+     </div>
+   </div> 
+     }
     </div>
   );
 };
