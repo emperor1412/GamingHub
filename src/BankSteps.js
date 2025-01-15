e10fb711e379a8e1d3bb373624deae7a042bbe61
admin <admin@home.com>
2025-01-15 15:37:36 +0700

    bank step overlay claim success

--- src/BankSteps.js
+++ src/BankSteps.js
@@ -3,12 +3,21 @@ import './BankSteps.css';
 import shared from './Shared';
 import backIcon from './images/back.svg';
 import sneaker_icon from './images/sneaker_icon.png';
+import bank_step_coin from './images/bank_step_coin.png';
 import bank_step_background from './images/bank_step_background.png';
 import checkmark from './images/checkmark_banksteps.png';
+import star1 from './images/single_star.svg';
+import star2 from './images/single_star_2.svg';
+import star3 from './images/single_star_3.svg';
+import star4 from './images/single_star_4.svg';
+import star5 from './images/single_star_5.svg';
+import starlet from './images/starlet.png';
+import { shareStory } from '@telegram-apps/sdk';
 
 const BankSteps = ({ showFSLIDScreen, onClose }) => {
 
-    const [canClaim, setCanClaim] = React.useState(false);
+    const [canClaim, setCanClaim] = React.useState(true);
+    const [showOverlayClaimSuccess, setShowOverlayClaimSuccess] = React.useState(true);
 
     const handleBack = () => {
         onClose();
@@ -20,11 +29,77 @@ const BankSteps = ({ showFSLIDScreen, onClose }) => {
 
     const handleClaimStarlets = () => {
         // Handle claiming starlets
+        setShowOverlayClaimSuccess(true);
     };
 
     const handleFindOutMore = () => {
         // Handle find out more action
         window.open("https://youtu.be/ZmEq4LLxRnw?si=1z635ok5An4u_HeV", "_blank");
+    };
+
+    const claimRewardFromSharingStory = async (depth = 0) => {
+        if (depth > 3) {
+            console.error('claimRewardFromSharingStory failed after 3 attempts');
+            return;
+        }
+
+        console.log('Claiming reward from sharing story...');
+
+        try {
+            const response = await fetch(`${shared.server_url}/api/app/sharingStory?token=${shared.loginData.token}&type=0`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Claim reward from sharing story data:', data);
+
+                if (data.code === 0) {
+                    console.log('Reward claimed successfully');
+                }
+                else if (data.code === 102002 || data.code === 102001) {
+                    console.error('Claim reward from sharing story error:', data.msg);
+                    const result = await shared.login(shared.initData);
+                    if (result.success) {
+                        claimRewardFromSharingStory(depth + 1);
+                    }
+                    else {
+                        console.error('Login failed:', result.error);
+                    }
+                }
+                else {
+                    console.error('Claim reward from sharing story error:', response);
+                }
+            }
+            else {
+                console.error('Claim reward from sharing story error:', response);
+            }
+        }
+        catch (error) {
+            console.error('claimRewardFromSharingStory error:', error);
+        }
+    };
+
+    const onClickShareStory = async () => {
+        console.log('Share story');
+
+        if (shareStory.isSupported()) {
+            const inviteLink = `${shared.app_link}?startapp=invite_${shared.loginData.link}`;
+            try {
+                await shareStory({
+                    media: 'https://storage.googleapis.com/text2image-118de.appspot.com/Test/FSL.png',
+                    text: inviteLink,
+                    button_text: 'Join Now',
+                });
+                await claimRewardFromSharingStory();
+                setShowOverlayClaimSuccess(false);
+            } catch (error) {
+                console.error('Error sharing story:', error);
+            }
+        }
     };
 
     if (shared.userProfile.fslId === 0 && false) {
@@ -153,6 +228,47 @@ const BankSteps = ({ showFSLIDScreen, onClose }) => {
                     </button>
                 </div>
             </div>
+            {showOverlayClaimSuccess && (
+                <div className="bank-step-overlay">
+                    <div className="bank-step-overlay-content">
+                        <div className="bank-step-overlay-images">
+                            <img src={bank_step_coin} alt="" className="bank-step-overlay-coin" />
+                            <img src={sneaker_icon} alt="" className="bank-step-overlay-sneaker" />
+                            <div className="bank-step-sparkle top-left"></div>
+                            <div className="bank-step-sparkle top-right"></div>
+                            <div className="bank-step-sparkle bottom-left"></div>
+                            <div className="bank-step-sparkle bottom-right"></div>
+                            <img src={star1} alt="" className="bank-step-star star1" />
+                            <img src={star2} alt="" className="bank-step-star star2" />
+                            <img src={star3} alt="" className="bank-step-star star3" />
+                            <img src={star4} alt="" className="bank-step-star star4" />
+                            <img src={star5} alt="" className="bank-step-star star5" />
+                        </div>
+                        <div className="bank-step-overlay-text">
+                            <div>STARLETS CLAIMED</div>
+                            <div>SUCCESSFULLY!</div>
+                        </div>
+
+                        {/* <button 
+                            className="action-button share-story-button" 
+                            onClick={onClickShareStory}
+                        >
+                            <span>SHARE TO STORY</span>
+                            <div className="bank-step-share-story-reward">
+                                <img src={starlet} alt="" className="share-story-reward-icon" />
+                                <span>24.4</span>
+                            </div>
+                        </button> */}
+
+                        <button 
+                            className="action-button" 
+                            onClick={() => setShowOverlayClaimSuccess(false)}
+                        >
+                            <span>DONE</span>
+                        </button>
+                    </div>
+                </div>
+            )}
         </>
     );
 };
