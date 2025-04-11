eb194dde1249fd5bdadd6ff7eb837a81437400f3
vuongnt2201 <vuongnt2201@gmail.com>
2025-04-11 15:09:25 +0700

    fix minor layout

--- src/MainView.js
+++ src/MainView.js
@@ -53,7 +53,7 @@ const MainView = ({ checkInData, setShowCheckInAnimation, checkIn, setShowCheckI
     const [showedEggletToday, setShowedEggletToday] = useState(false);
     
     // Set to true to disable daily checking and always show popup when event is active
-    const isMockup = true;
+    const isMockup = false;
 
     // const [scrollLeft, setScrollLeft] = useState(0);
     // const [startX, setStartX] = useState(0);
