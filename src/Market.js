3c04df9c105c85c27f82c37cab7bb191e4421530
vuongnt2201 <vuongnt2201@gmail.com>
2025-09-04 15:00:45 +0700

    update show starlet price when not enough

--- src/Market.js
+++ src/Market.js
@@ -966,7 +966,7 @@ const Market = ({ showFSLIDScreen, setShowProfileView, initialTab = 'telegram' }
                         // 4. Not enough starlets
                         else if (userStarlets < product.starlet) {
                           isDisabled = true;
-                          disabledReason = product.starlet.toString();
+                          disabledReason = product.starlet.toLocaleString() + ' STARLETS';
                         }
                         // 5. Limit reached
                         else if (product.purchasedQuantity >= product.limitNum) {
