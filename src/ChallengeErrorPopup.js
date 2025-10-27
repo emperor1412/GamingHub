4399b436edb9f51dbf59ae9e84ec3d2b8ff27909
vuongnt01 <vuongnt01@gmail.com>
2025-10-27 22:29:25 +0700

    update challenge not done popup

--- src/ChallengeErrorPopup.js
+++ src/ChallengeErrorPopup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ChallengeErrorPopup.css';
+import errorImage from './images/challenge_not_done.png';
 
 const ChallengeErrorPopup = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
@@ -9,19 +10,22 @@ const ChallengeErrorPopup = ({ isOpen, onClose }) => {
       <div className="cep-popup" onClick={(e) => e.stopPropagation()}>
         {/* Error Header */}
         <div className="cep-error-header">
-          <span className="cep-error-text">ERROR</span>
+          <span className="cep-error-text">CHALLENGE NOT COMPLETE</span>
         </div>
         
         {/* Main Message */}
         <div className="cep-message-container">
-          <div className="cep-message-group">
-            <div className="cep-message-line">THIS CHALLENGE</div>
-            <div className="cep-message-line">IS NOT FINISHED</div>
+          <div className="cep-message-image">
+            <img src={errorImage} alt="Error" />
           </div>
           <div className="cep-message-group">
-            <div className="cep-message-line">BANK MORE STEPS</div>
-            <div className="cep-message-line">TO FINISH THE</div>
-            <div className="cep-message-line">CHALLENGE</div>
+            <div className="cep-message-line">YOU HAVEN'T </div>
+            <div className="cep-message-line">REACHED THE GOAL.</div>
+          </div>
+          <div className="cep-message-group">
+            <div className="cep-message-line">BANK YOUR LATEST</div>
+            <div className="cep-message-line">STEPS TO UPDATE</div>
+            <div className="cep-message-line">YOUR PROGRESS.</div>
           </div>
         </div>
         
